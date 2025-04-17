7140b0195a6b1fdac3b937fea75da8013b80c3d1
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-17 18:01:29 -0500

    first commit

---
+++ src/datos/ListaEstudiantes.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+import java.io.*;
+import java.util.*;
+import entidades.*;
+/**
+ *
+ * @author USER
+ */
+public class ListaEstudiantes {
+    private final String archivo= "Estudiantes.txt";
+    
+    public void agregarEstudiante (Estudiante nuevo){
+        ArrayList <Estudiante> lista = leerEstudiantes();
+        lista.add(nuevo);
+        guardarEstudiantes(lista);
+    }
+    
+    public ArrayList<Estudiante> leerEstudiantes(){
+        ArrayList<Estudiante> lista = new ArrayList<>();
+        File f = new File (archivo);
+        if(!f.exists()) return lista;
+        try (ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(archivo))) {
+            lista = (ArrayList<Estudiante>) entrada.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("❌ Error al leer: " + e.getMessage());
+        }
+
+        return lista;
+    }
+    
+    public void guardarEstudiantes(ArrayList <Estudiante> lista){
+        try (ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(archivo))) {
+            salida.writeObject(lista);
+        } catch (IOException e) {
+            System.out.println("❌ Error al guardar: " + e.getMessage());
+        }
+    }
+}
---
+++ src/entidades/Estudiante.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+import java.io.*;
+/**
+ *
+ * @author USER
+ */
+public class Estudiante implements Serializable{
+    String nombre;
+    String codigo;
+    String escuela;
+
+    public Estudiante(String nombre, String codigo, String escuela) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.escuela = escuela;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "Nombres y apellidos=" + nombre + ", Codigo de matricula=" + codigo + ", "
+                + "Escuela profesional=" + escuela + '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getEscuela() {
+        return escuela;
+    }
+}
