6ba4293d776d79cf8ab489fa92e006823e5f889d
Calderon Algalobos Roberto David <rdcalderon@unitru.edu.pe>
2025-04-17 23:56:16 -0500

    Clase ListaDocentes

---
+++ src/datos/ListaDocentes.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+import java.io.*;
+import java.util.*;
+import entidades.*;
+
+/**
+ *
+ * @author Administrador
+ */
+public class ListaDocentes {
+    
+    public ArrayList<Docente> leerDocentes() {
+        ArrayList<Docente> listaD = new ArrayList<>();
+        File archivo = new File("Registro Docente.txt");
+        if(archivo.exists())
+            return listaD;
+        try(ObjectInputStream ingreso = new ObjectInputStream(new FileInputStream("Registro Docente.txt"))) {
+            listaD = (ArrayList<Docente>) ingreso.readObject();
+        }catch(IOException e){
+            System.out.println("Ha ocurrido un error en la lectura: " + e.getMessage());
+        }catch(ClassNotFoundException e){
+            System.out.println("Ha ocurrido un error en la lectura: " + e.getMessage());
+        }
+ 
+        return listaD;
+    }
+}
