a6f20868916be4bea36da052168b1f831aca3a01
Calderon Algalobos Roberto David <rdcalderon@unitru.edu.pe>
2025-04-17 22:00:58 -0500

    Creacion clase abstracta Persona

---
+++ src/entidades/Persona.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Administrador
+ */
+public abstract class Persona {
+    private String nombre;
+    private String apellido;
+    private String Dni;
+
+    public Persona() {
+        this("NN", "NA","ND");
+    }
+
+    public Persona(String nombre, String apellido, String Dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Dni = Dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setDni(String Dni) {
+        this.Dni = Dni;
+    }
+
+    public String getDni() {
+        return Dni;
+    }
+    
+    @Override
+    public String toString() {
+        return getNombre() + " " + getApellido() + " " + 
+                getDni();
+    }
+}
